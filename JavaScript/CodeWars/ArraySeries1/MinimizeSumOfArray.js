764864f5e317ab4991c9093e39bc3b0013c808ab
Aissa Feldmann <aissafeldmann@Aissas-Air.attlocal.net>
2020-11-20 13:41:37 -0500

    array series added

---
+++ JavaScript/CodeWars/ArraySeries1/MinimizeSumOfArray.js
@@ -0,0 +1,13 @@
+/**Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product . */
+function minSum(arr) {
+  arr.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < arr.length / 2; i++) {
+    let product = arr[i] * arr[arr.length - (i + 1)];
+    console.log(product);
+    sum += product;
+  }
+  return sum;
+}
+let result = minSum([9, 2, 8, 7, 5, 4, 0, 6]);
+console.log(result);
