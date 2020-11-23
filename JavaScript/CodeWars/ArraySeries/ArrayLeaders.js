5855b3755f6c6ed2f881b793ba9d58d5998c0010
Aissa Feldmann <aissaf@umich.edu>
2020-11-23 00:00:14 -0500

    added ArrayLeaders

--- JavaScript/CodeWars/ArraySeries/ArrayLeaders.js
+++ JavaScript/CodeWars/ArraySeries/ArrayLeaders.js
@@ -8,9 +8,26 @@ Repetition of numbers in the array/list could occur.
 
 Returned Array/list should store the leading numbers in the same order in the original array/list */
 
-const arrLeaders = (array) => {
-    
-}
+const arrayLeaders = (numbers, leaders = []) => {
+  for (let i = 0; i < numbers.length; i++) {
+    // if (i === numbers.length - 1 && numbers[i] > 0) {
+    //   leaders.push(numbers[i]);
+    //   return leaders;
+    // }
+    // if (i === numbers.length - 1 && numbers[i] <= 0) {
+    //   return leaders;
+    // }
+
+    // Adding '0' as 'initial value' (second argument in reducer) eliiminates 
+    // need for lines 13-19. 
+    // "Calling reduce() on an empty array without an initialValue will throw a
+    // TypeError." - MDN
+    if (numbers[i] > numbers.slice(i + 1).reduce((acc, curr) => acc + curr, 0)) {
+      leaders.push(numbers[i]);
+    }
+  }
+  return leaders;
+};
 
 let results = arrayLeaders([1,2,3,4,0]);
 console.log(results);
