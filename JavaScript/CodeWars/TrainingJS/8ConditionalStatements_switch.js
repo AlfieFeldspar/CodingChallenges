97752688635f867e6fa4117b7ad2575a94afd1c3
Aissa Feldmann <aissaf@umich.edu>
2020-11-26 11:57:35 -0500

    refactored js switch

--- JavaScript/CodeWars/TrainingJS/8ConditionalStatements_switch.js
+++ JavaScript/CodeWars/TrainingJS/8ConditionalStatements_switch.js
@@ -1,17 +1,17 @@
 /** Complete function howManydays, function accepts 1 parameter: month (numerical). Return a number of days in this month (month is always greater than 0, less than or equal to 12).*/
 
 function howManydays(month) {
-  let days;
   switch (month) {
     case 2:
-      return (days = 28);
+      return 28;
+    //these can live on one line too
     case 4:
     case 6:
     case 9:
     case 11:
-      return (days = 30);
+      return 30;
     default:
-      return (days = 31);
+      return 31;
   }
   //alternative to using default case
   //return days = 31;
