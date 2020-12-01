a4ac1f5620e8fc72f62f1d5266dea88788bcefe4
Aissa Feldmann <aissaf@umich.edu>
2020-12-01 18:45:35 -0500

    generate range complete

--- JavaScript/CodeWars/GenerateRangeOfIntegers.js
+++ JavaScript/CodeWars/GenerateRangeOfIntegers.js
@@ -1,7 +1,13 @@
-/**Implement a function named generateRange(min, max, step) which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max) */
+/**Implement a function named generateRange(min, max, step) which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+ * Returns an array
+ * Max does not need to be in the range, depending on the step*/
 
-const generateRange = (min, max, step) => {
-
+function generateRange(min, max, step) {
+  let answer = [];
+  for (let i = min; i <= max; i+=step) {
+    answer.push(i);
+  }
+  return answer;
 }
 
 let results = generateRange(2, 10, 2);
