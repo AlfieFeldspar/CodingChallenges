aa0783c84835c2e59a38061e83fbd81de95cce21
Aissa Feldmann <aissaf@umich.edu>
2020-12-06 14:56:40 -0500

    nested boxes done

--- JavaScript/HackerRank/TenDaysOfJavaScript/RegExp1.js
+++ JavaScript/HackerRank/TenDaysOfJavaScript/RegExp1.js
@@ -1,13 +1,20 @@
 /**Complete the function in the editor below by returning a RegExp object, 're', that matches any string 's' that begins and ends with the same vowel. Recall that the English vowels are a, e, i, o, and u.*/
 
-function regexVar() {
-    /*
-     * Declare a RegExp object variable named 're'
-     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
-     */
-    let re = /([aeiou]).*\1/
-    return re;
-}
+function regexVar(str) {
+    //  ^ => first item matches:
+    // () => stores matching value captured within
+    // [aeiou] => matches any of the characters in the brackets
+    // . => matches any character:
+    // + => for 1 or more occurrances (this ensures str length > 3)
+    // \1 => matches to previously stored match. 
+        // \2 looks for matched item stored 2 instances ago 
+        // \3 looks for matched item stored 3 ago, etc
+    
+    //  $ ensures that matched item is at end of the sequence
+    
+        let re = /^([aeiou]).+\1$/;
+        return re;
+    }
 
 // regex surrounded by /   /
 // (.) would be any first character. in this case ([aeiou]) is any vowel.
@@ -16,3 +23,5 @@ function regexVar() {
 
 //returns 'false' for 'bcb'
 //returns 'true' for 'asoiudfa'
+
+regexVar('aewxyzae')
