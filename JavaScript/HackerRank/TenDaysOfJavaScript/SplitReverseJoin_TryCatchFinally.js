6b1d99c89e2d415833a4ed7a88e23bcfc6aa6baf
Aissa Feldmann <aissaf@umich.edu>
2020-12-06 18:18:30 -0500

    split, reverse, join, try, catch, finally

--- JavaScript/HackerRank/TenDaysOfJavaScript/SplitReverseJoin_TryCatchFinally.js
+++ JavaScript/HackerRank/TenDaysOfJavaScript/SplitReverseJoin_TryCatchFinally.js
@@ -4,3 +4,16 @@ Try to reverse string s using the split, reverse, and join methods.
 If an exception is thrown, catch it and print the contents of the exception's message on a new line.
 Print s on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.*/
 
+function reverseString(s) {
+    let reversed;
+    try {
+        reversed = (s.split("").reverse().join(""));
+    }
+    catch(e) {
+        console.log(e.message);
+    }
+    finally {
+        console.log(reversed? reversed: s)
+    }
+}
+reverseString('1234')
