3d3f3b172246bf1f5029f0e7b8d1c3d686830bf0
Aissa Feldmann <aissaf@umich.edu>
2020-12-06 16:19:51 -0500

    if else

---
+++ JavaScript/HackerRank/TenDaysOfJavaScript/IF_ELSE.js
@@ -0,0 +1,15 @@
+function getGrade(score) {
+  let grade = "";
+  score <= 5
+    ? (grade = "F")
+    : score > 5 && score <= 10
+    ? (grade = "E")
+    : score > 10 && score <= 15
+    ? (grade = "D")
+    : score > 15 && score <= 20
+    ? (grade = "C")
+    : score > 20 && score <= 25
+    ? (grade = "B")
+    : (grade = "A");
+  return grade;
+}
