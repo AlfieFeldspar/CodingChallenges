281feecf524c8c16abaf1ee4adf7ca5bce5ba984
Aissa Feldmann <aissaf@umich.edu>
2020-12-08 13:50:02 -0500

    array map

---
+++ JavaScript/HackerRank/TenDaysOfJavaScript/Map.js
@@ -0,0 +1,14 @@
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+ let result = nums.map(num => {
+        return num%2===0?num*2:num*3;
+    })
+    console.log(result)
+}
+
+modifyArray([1,2,3,4,5])
